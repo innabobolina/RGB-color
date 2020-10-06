a533c68d2de316c4251b9525407e73d34ff0adb9
Inna Botchkina <innabobolina@yahoo.com>
2020-10-06 16:45:21 -0700

    display if a clicked square is correct

--- color.js
+++ color.js
@@ -1,9 +1,9 @@
 let colors = [
-  "rgb(255, 0, 0)",
+  "rgb(255, 0, 0)", // red
   "rgb(255, 255, 0)", // yellow
   "rgb(0, 255, 0)", // lime
   "rgb(0, 255, 255)", // cyan/aqua
-  "rgb(0, 0, 255)",
+  "rgb(0, 0, 255)", // blue
   "rgb(255, 0, 255)", // magenta
 ];
 
@@ -13,8 +13,12 @@ for (let i = 0; i < squares.length; i++) {
   squares[i].style.background = colors[i];
   squares[i].addEventListener("click", function () {
     let clickedColor = this.style.background;
-    if (clickedColor === pickedColor) alert("Correct!");
-    else alert("Wrong!");
+    if (clickedColor === pickedColor)
+      document.getElementById("correctOrNot").textContent = "Correct";
+    else {
+      this.style.background = "#232323";
+      document.getElementById("correctOrNot").textContent = "Try Again!";
+    }
   });
 }
 
